c69ab12f9dcb03a89264230149444b04f5528fc3
lkaijie <kaijie6@ualberta.ca>
2023-05-16 17:42:06 -0600

    star on sid edesign

--- app/src/main/java/com/example/hsrguide/ui/characters/CharacterAdapter.kt
+++ app/src/main/java/com/example/hsrguide/ui/characters/CharacterAdapter.kt
@@ -3,11 +3,14 @@ package com.example.hsrguide.ui.characters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hsrguide.R
 import com.example.hsrguide.databinding.ItemCharacterBinding
+import com.example.hsrguide.data.model.Character
 
-class CharacterAdapter(private val characters: List<Char>): RecyclerView.Adapter<CharacterAdapter.CharacterViewHolder>() {
+
+class CharacterAdapter(private val characters: List<Character>): RecyclerView.Adapter<CharacterAdapter.CharacterViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
         val itemView = LayoutInflater.from(parent.context)
@@ -25,80 +28,85 @@ class CharacterAdapter(private val characters: List<Char>): RecyclerView.Adapter
     }
 
     class CharacterViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        fun bind(character: com.example.hsrguide.data.model.Character) {
+        fun bind(character: Character) {
             val binding = ItemCharacterBinding.bind(itemView)
             binding.apply {
                 characterName.text = character.name
+                characterFaction.text = "Faction: "+ character.faction
 
-
+                val resourceName = character.name.lowercase() + "_big"
+                val resId = itemView.context.resources.getIdentifier(resourceName, "drawable", itemView.context.packageName)
+                characterImg.setImageResource(resId)
 
                 if (character.rarity == "4"){
                     rarityStar5.visibility = View.GONE
+                    binding.root.setBackgroundResource(R.drawable.four_star_background)
+
                 }
                 when (character.path){
                     "Nihility" -> {
-                        characterPath.text = "Path: "+ Character.path
+                        characterPath.text = "Path: "+ character.path
                         pathIcon.setImageResource(R.drawable.nihility_hsr)
                     }
                     "Destruction" -> {
-                        characterPath.text = "Path: "+ Character.path
+                        characterPath.text = "Path: "+ character.path
                         pathIcon.setImageResource(R.drawable.destruction_hsr)
                     }
                     "Erudition" -> {
-                        characterPath.text = "Path: "+ Character.path
+                        characterPath.text = "Path: "+ character.path
                         pathIcon.setImageResource(R.drawable.erudition_hsr)
                     }
                     "Harmony" -> {
-                        characterPath.text = "Path: "+ Character.path
+                        characterPath.text = "Path: "+ character.path
                         pathIcon.setImageResource(R.drawable.harmony_hsr)
                     }
                     "Preservation" -> {
-                        characterPath.text = "Path: "+ Character.path
+                        characterPath.text = "Path: "+ character.path
                         pathIcon.setImageResource(R.drawable.preservation_hsr)
                     }
                     "Abundance" -> {
-                        characterPath.text = "Path: "+ Character.path
+                        characterPath.text = "Path: "+ character.path
                         pathIcon.setImageResource(R.drawable.abundance_hsr)
                     }
                     "Hunt" -> {
-                        characterPath.text = "Path: "+ Character.path
+                        characterPath.text = "Path: "+ character.path
                         pathIcon.setImageResource(R.drawable.hunt_hsr)
                     }
 
                 }
                 when (character.element){
                     "Wind" -> {
-                        characterElementText.text = "Element: "+ Character.element
+                        characterElementText.text = "Element: "+ character.element
                         characterElementImg.setImageResource(R.drawable.wind_hsr)
                         characterElement.setImageResource(R.drawable.wind_hsr)
                     }
                     "Fire" -> {
-                        characterElementText.text = "Element: "+ Character.element
+                        characterElementText.text = "Element: "+ character.element
                         characterElementImg.setImageResource(R.drawable.fire_hsr)
                         characterElement.setImageResource(R.drawable.fire_hsr)
                     }
                     "Ice" -> {
-                        characterElementText.text = "Element: "+ Character.element
+                        characterElementText.text = "Element: "+ character.element
                         characterElementImg.setImageResource(R.drawable.ice_hsr)
                         characterElement.setImageResource(R.drawable.ice_hsr)
                     }
                     "Quantum" -> {
-                        characterElementText.text = "Element: "+ Character.element
+                        characterElementText.text = "Element: "+ character.element
                         characterElementImg.setImageResource(R.drawable.quantum_hsr)
                         characterElement.setImageResource(R.drawable.quantum_hsr)
                     }
                     "Imaginary" -> {
-                        characterElementText.text = "Element: "+ Character.element
+                        characterElementText.text = "Element: "+ character.element
                         characterElementImg.setImageResource(R.drawable.imaginary_hsr)
                         characterElement.setImageResource(R.drawable.imaginary_hsr)
                     }
                     "Physical" -> {
-                        characterElementText.text = "Element: "+ Character.element
+                        characterElementText.text = "Element: "+ character.element
                         characterElementImg.setImageResource(R.drawable.physical_hsr)
                         characterElement.setImageResource(R.drawable.physical_hsr)
                     }
                     "Lightning" -> {
-                        characterElementText.text = "Element: "+ Character.element
+                        characterElementText.text = "Element: "+ character.element
                         characterElementImg.setImageResource(R.drawable.lightning_hsr)
                         characterElement.setImageResource(R.drawable.lightning_hsr)
                     }
--- app/src/main/java/com/example/hsrguide/ui/characters/Characters.kt
+++ app/src/main/java/com/example/hsrguide/ui/characters/Characters.kt
@@ -6,8 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.hsrguide.R
 import com.example.hsrguide.databinding.CharactersMainBinding
+import com.example.hsrguide.data.model.Character
+import com.example.hsrguide.ui.characters.GridSpacingItemDecoration
+
 
 class Characters : Fragment() {
     private var _binding: CharactersMainBinding? = null
@@ -15,6 +19,26 @@ class Characters : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = CharactersMainBinding.inflate(inflater, container, false)
+
+
+        // set up the recycler view
+        val recyclerView = binding.charactersRecyclerView
+        val layoutManager = GridLayoutManager(requireContext(), 2)
+        recyclerView.layoutManager = layoutManager
+        val charactersList = listOf(
+            Character("SilverWolf", "image_url_1", "5", "Quantum", "TEst", "Nihility"),
+            Character("Tingyun", "image_url_2", "4", "Lightning", "Faction 2", "Harmony"),
+            // Add more characters as needed
+        )
+        val adapter = CharacterAdapter(charactersList)
+        recyclerView.adapter = adapter
+
+        val spanCount = 2 // Number of columns in the grid
+        val spacing = resources.getDimensionPixelSize(R.dimen.grid_spacing) // Spacing between items in pixels
+        val includeEdge = true // Include spacing at the edges of the grid
+
+        recyclerView.addItemDecoration(GridSpacingItemDecoration(spanCount, spacing, includeEdge))
+
         return binding.root
     }
 
